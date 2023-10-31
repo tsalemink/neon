7a2beb7749a13a0c1e9a252c3b81e4777a250111
TimmyNic <tim.nicolas@outlook.com>
2023-10-31 17:52:32 +1300

    Add script to create binary application
    Adds a Python script utilizing PyInstaller to create a Neon binary application.

---
+++ res/pyinstaller/create_application.py
@@ -0,0 +1,57 @@
+import argparse
+import os
+import platform
+
+import PySide6 as RefMod
+
+import PyInstaller.__main__
+
+from cmapps.neon.settings.mainsettings import APPLICATION_NAME
+
+
+# Set Python optimisations on.
+os.environ['PYTHONOPTIMIZE'] = '1'
+
+here = os.path.dirname(__file__)
+
+
+def main():
+    run_command = [
+        '../../src/cmapps/neon/neon.py',
+        '-n', f'{APPLICATION_NAME}',
+        '--windowed',
+        '--noconfirm',
+        '--hidden-import', 'cmlibs.zinc',
+        '--hidden-import', 'cmlibs.argon',
+        '--hidden-import', 'cmlibs.utils',
+        '--hidden-import', 'cmlibs.widgets',
+    ]
+
+    pyside_dir = os.path.dirname(RefMod.__file__)
+
+    if platform.system() == 'Darwin':
+        rcc_exe = os.path.join(pyside_dir, 'Qt', 'libexec', "rcc")
+        uic_exe = os.path.join(pyside_dir, 'Qt', 'libexec', "uic")
+
+    elif platform.system() == "Windows":
+        rcc_exe = os.path.join(pyside_dir, "rcc.exe")
+        uic_exe = os.path.join(pyside_dir, "uic.exe")
+
+    else:
+        raise NotImplementedError("Platform is not supported for creating a Neon application.")
+
+    icon = os.path.join('..', 'images', 'icons', 'neon-icon.png')
+    run_command.append(f'--icon={icon}')
+
+    run_command.append(os.pathsep.join([f'--add-binary={rcc_exe}', 'PySide6/']))
+    run_command.append(os.pathsep.join([f'--add-binary={uic_exe}', 'PySide6/']))
+
+    print('Running command: ', run_command)
+    PyInstaller.__main__.run(run_command)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="create_application")
+    args = parser.parse_args()
+
+    main()
